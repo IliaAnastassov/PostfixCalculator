3ba4ed19156a662fb2e98c774c9db399e276152c
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-08-31 18:52:30 +0300

    Initial Commit

---
+++ PostfixCalculator/PostfixCalculator/PostfixCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostfixCalculator
+{
+    public class PostfixCalculator
+    {
+        private const string ADDITION = "+";
+        private const string SUBTRACTION = "-";
+        private const string MULTIPLICATION = "*";
+        private const string DIVISION = "/";
+        private const string MODULUS = "%";
+
+        private Stack<int> values;
+
+        public PostfixCalculator()
+        {
+            values = new Stack<int>();
+        }
+
+        public int Calculate(IEnumerable<string> tokens)
+        {
+            foreach (var token in tokens)
+            {
+                if (int.TryParse(token, out int number))
+                {
+                    values.Push(number);
+                }
+                else
+                {
+                    var rightOperand = values.Pop();
+                    var leftOperand = values.Pop();
+
+                    if (token == ADDITION)
+                    {
+                        var result = leftOperand + rightOperand;
+                        values.Push(result);
+                    }
+                    else if (token == SUBTRACTION)
+                    {
+                        var operationResult = leftOperand - rightOperand;
+                        values.Push(operationResult);
+                    }
+                    else if (token == MULTIPLICATION)
+                    {
+                        var result = leftOperand * rightOperand;
+                        values.Push(result);
+                    }
+                    else if (token == DIVISION)
+                    {
+                        var result = leftOperand / rightOperand;
+                        values.Push(result);
+                    }
+                    else if (token == MODULUS)
+                    {
+                        var result = leftOperand % rightOperand;
+                        values.Push(result);
+                    }
+                }
+            }
+
+            var finalResult = values.Pop();
+            return finalResult;
+        }
+    }
+}
---
+++ PostfixCalculator/PostfixCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PostfixCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            foreach (var token in args)
+            {
+                int number;
+
+                if (int.TryParse(token, out number))
+                {
+
+                }
+            }
+        }
+    }
+}
